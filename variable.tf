f3a05819acfcb334d253d801ed2d6220a716a668
tsarjun <110442994+tsarjun@users.noreply.github.com>
2026-01-20 23:24:19 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -45,24 +45,8 @@ type = string
 default = "Azure clint ID"
 type = string 
 }
- variable "client_secret" {
-description ="Azure client secret"
-type = string
-}
-variable "client_id" {
-  description = "Azure client ID"
+ variable "ssh_public_key" {
+  description = "SSH public key for Azure VM"
   type        = string
-  sensitive   = true
 }
 
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  sensitive   = true
-}
