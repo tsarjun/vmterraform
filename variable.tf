34a7635766757a2e17cb1d57f1725ac1ecbe7d1d
arjun <tsarjungowda@gmail.com>
2026-01-20 09:09:26 -0800

    added file

--- variable.tf
+++ variable.tf
@@ -9,7 +9,7 @@ type = string
 }
 
 variable "address_space" {
-default = [10.0.1.0/16"]
+default = ["10.0.1.0/16"]
 type = list(string)
 }
  variable "vnet"{
@@ -17,8 +17,8 @@ default = "arvnet"
 type = string 
 }
 
-variable "address prefixes"{
-default = [10.0.1.0/24"]
+variable "address_prefixes"{
+default = ["10.0.1.0/24"]
 type = list(string)
 }
  
